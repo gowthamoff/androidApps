60a490f3baea2834f59f658f57d1ab344d2c5233
Gowtham <127670808+gowthamoff@users.noreply.github.com>
2023-09-29 14:03:46 +0530

    google sign in commit

---
+++ FormApp/app/src/main/java/login/form/GoogleActivity.java
@@ -0,0 +1,13 @@
+package login.form;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GoogleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google);
+    }
+}
--- FormApp/app/src/main/java/login/form/MainActivity.java
+++ FormApp/app/src/main/java/login/form/MainActivity.java
@@ -9,7 +9,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -20,18 +22,42 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final int RC_SIGN_IN = 1000;
     private TextView dateTextView;
     private ImageView selectedImageView;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_IMAGE_PICK = 2;
+
+    Button googleAuth;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +70,29 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("UserData", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
+        googleAuth = findViewById(R.id.btnGoogleAuth);
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        googleAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleSignIn();
+            }
+        });
         // Set "ProfileImage" to an empty string by default
         editor.putString("ProfileImage", "");
         editor.apply();
+
     }
 
+    private void googleSignIn(){
+        Intent intent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(intent,RC_SIGN_IN);
+    }
     private boolean areEditTextsEmpty() {
         EditText[] editTexts = {
                 findViewById(R.id.editfirst),
@@ -213,9 +257,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            if(requestCode == RC_SIGN_IN)
+            {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    firbaseAuth(account.getIdToken());
+                } catch (ApiException e) {
+                    // Handle specific Google Sign-In API exceptions
+                    Log.e("YourTag", "Google Sign-In API Exception: " + e.getStatusCode());
+                    Toast.makeText(this, "Google Sign-In Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    // Handle other exceptions
+                    Log.e("YourTag", "Exception: " + e.getMessage(), e);
+                    Toast.makeText(this, "An error occurred: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
 
         if (resultCode == RESULT_OK) {
             if (requestCode == REQUEST_IMAGE_CAPTURE && data != null) {
@@ -244,4 +305,26 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+            private void firbaseAuth(String idToken) {
+                AuthCredential credential = GoogleAuthProvider.getCredential(idToken,null);
+                auth.signInWithCredential(credential)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task){
+                                if(task.isSuccessful()){
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    HashMap<String,Object> map = new HashMap<>();
+                                    map.put("id",user.getUid());
+                                    map.put("name",user.getDisplayName());
+                                    map.put("profile",user.getPhotoUrl().toString());
+                                    database.getReference().child("users").child(user.getUid()).setValue(map);
+                                    Intent intent = new Intent(MainActivity.this,GoogleActivity.class);
+                                    startActivity(intent);
+                                }else{
+                                    Toast.makeText(MainActivity.this,"something went wrong ",Toast.LENGTH_SHORT).show();
+                                }
+                        }
+                    });
+        }
 }
